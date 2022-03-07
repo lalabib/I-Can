3d8241501a4279b5d5e5fd42cfdc897deddde380
lalabib <labibla21@gmail.com>
2022-03-07 19:41:12 +0700

    ICan: initial servicer for networking

---
+++ app/src/main/java/com/project/lalabib/ican/service/ApiClient.kt
@@ -0,0 +1,29 @@
+package com.project.lalabib.ican.service
+
+import com.project.lalabib.ican.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+
+    private const val baseUrl = BuildConfig.URL
+
+    val instance: ApiEndPoint by lazy {
+        val interceptor = if(BuildConfig.DEBUG) {
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        } else {
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+        }
+        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(ApiEndPoint::class.java)
+    }
+}
---
+++ app/src/main/java/com/project/lalabib/ican/service/ApiEndPoint.kt
@@ -0,0 +1,12 @@
+package com.project.lalabib.ican.service
+
+import com.project.lalabib.ican.data.lcoal.FishEntity
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiEndPoint {
+
+    @GET("storages/5e1edf521073e315924ceab4/list")
+    fun getFish(@Query("limit") limit: String): Call<List<FishEntity>>
+}
