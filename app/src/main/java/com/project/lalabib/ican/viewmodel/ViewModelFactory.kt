13ab28a525e897e8d829f0ef87ccf631aca5e11e
lalabib <labibla21@gmail.com>
2022-03-07 19:56:54 +0700

    HomeViewModel: initial code initial code for HomeViewModel and adding ViewModelFactory

---
+++ app/src/main/java/com/project/lalabib/ican/viewmodel/ViewModelFactory.kt
@@ -0,0 +1,32 @@
+package com.project.lalabib.ican.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.project.lalabib.ican.data.FishRepository
+import com.project.lalabib.ican.di.Injection
+import com.project.lalabib.ican.ui.home.HomeViewModel
+
+class ViewModelFactory private constructor(private val fishRepository: FishRepository)
+    : ViewModelProvider.NewInstanceFactory() {
+
+    companion object {
+        @Volatile
+        private var instance: ViewModelFactory? = null
+
+        fun getInstance(): ViewModelFactory =
+            instance ?: synchronized(this) {
+                instance
+                    ?: ViewModelFactory(Injection.provideRepository()).apply { instance = this }
+            }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(HomeViewModel::class.java) -> {
+                HomeViewModel(fishRepository) as T
+            }
+            else -> throw Throwable("Unknown ViewModel Class: " + modelClass.name)
+        }
+    }
+}
