fb6a1028fc2aace6318d4743ac2d9a82c3ea29d2
lalabib <labibla21@gmail.com>
2022-03-07 19:32:24 +0700

    ICan: initial entity

---
+++ app/src/main/java/com/project/lalabib/ican/data/lcoal/FishEntity.kt
@@ -0,0 +1,9 @@
+package com.project.lalabib.ican.data.lcoal
+
+data class FishEntity (
+    var uuid: String,
+    var komoditas: String,
+    var area_provinsi: String,
+    var size: String,
+    var price: String
+)
